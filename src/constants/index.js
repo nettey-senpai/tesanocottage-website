46a1270ee865b94141344364bf1ba7ae13ea889d
Jeffrey <netteyjeff@gmail.com>
2024-02-12 11:32:37 +0000

    initial commit

---
+++ src/constants/index.js
@@ -0,0 +1,8 @@
+export const navLinks = [
+  { href: "#home", label: "Home" },
+  { href: "#rooms", label: "Rooms" },
+  { href: "#restaurant", label: "Restaurant" },
+  { href: "#gallery", label: "Gallery" },
+  { href: "#events", label: "Events" },
+  { href: "#contact", label: "Contact" },
+];
---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    fontSize: {
+      xs: ["12px", "16px"],
+      sm: ["14px", "20px"],
+      base: ["16px", "19.5px"],
+      lg: ["18px", "21.94px"],
+      xl: ["20px", "24.38px"],
+      "2xl": ["24px", "29.26px"],
+      "3xl": ["28px", "50px"],
+      "4xl": ["48px", "58px"],
+      "8xl": ["96px", "106px"],
+    },
+    extend: {
+      fontFamily: {
+        palanquin: ["Palanquin", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+      colors: {
+        primary: "#ECEEFF",
+        "pomo-red": "#E05151",
+        "slate-gray": "#6D6D6D",
+        "pale-blue": "#F5F6FF",
+        "white-400": "rgba(255, 255, 255, 0.80)",
+      },
+      screens: {
+        xs: "480px",
+        ss: "620px",
+        sm: "768px",
+        md: "1060px",
+        lg: "1200px",
+        xl: "1700px",
+      },
+    },
+  },
+  plugins: [],
+};
