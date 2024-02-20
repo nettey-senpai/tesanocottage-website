b8a0609227e2717af34af4717831bec47c696a97
Jeffrey <netteyjeff@gmail.com>
2024-02-20 10:10:30 +0000

    update sections ui

--- src/sections/OurRooms.jsx
+++ src/sections/OurRooms.jsx
@@ -8,21 +8,21 @@ const OurRooms = () => (
     <div
       className={`${layout.sectionImgReverse} ${styles.flexStart} md:flex-row flex-col flex-wrap`}
     >
-      <div className="relative md:overflow-hidden  md:left-[-150px] md:top-[90px] md:z-0 shadow-3xl shadow-gray-400">
+      <div className="relative md:overflow-hidden  md:left-[-150px] md:top-[90px] md:z-0 z-10 shadow-3xl shadow-gray-400">
         <img
           src={room1}
           alt="room1"
           className="max-w-[300px] object-contain hover:scale-125 transition ease-in-out duration-1000"
         />
       </div>
-      <div className="relative md:overflow-hidden w-[300px] h-[220px] md:left-[100px] md:bottom-[70px] md:z-10 object-scale-down shadow-3xl shadow-gray-400">
+      <div className="relative md:overflow-hidden w-[300px] h-[220px] md:left-[100px] md:bottom-[70px] md:z-10 z-0 object-scale-down shadow-3xl shadow-gray-400">
         <img
           src={room2}
           alt="room2"
           className="w-[300px] h-[220px] object-cover hover:scale-125 transition ease-in-out duration-1000"
         />
       </div>
-      <div className="relative md:overflow-hidden md:right-[80px] md:bottom-[130px] md:z-20 shadow-3xl shadow-gray-400">
+      <div className="relative md:overflow-hidden md:right-[80px] md:bottom-[130px] md:z-20 z-10 shadow-3xl shadow-gray-400">
         <img
           src={room3}
           alt="room3"
