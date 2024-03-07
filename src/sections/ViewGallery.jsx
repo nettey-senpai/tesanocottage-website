5b7ed6cefd1cc8e4e5a5e95f11c2706f128e5a4d
Jeffrey <netteyjeff@gmail.com>
2024-03-07 10:05:08 +0000

    update home page sections

--- src/sections/ViewGallery.jsx
+++ src/sections/ViewGallery.jsx
@@ -8,7 +8,9 @@ const ViewGallery = () => (
   <section className={`${styles.paddingY} lg:px-2 px-4`}>
     {/* Title */}
     <div className="text-center">
-      <h2 className={`${styles.heading2}`}>Rooms, Restaurant & Events</h2>
+      <h2 className="font-palanquin font-semibold xs:text-[48px] text-[30px] xs:leading-[76.8px] leading-[66.8px] w-full">
+        See Gallery
+      </h2>
     </div>
 
     {/* Cards */}
@@ -20,14 +22,15 @@ const ViewGallery = () => (
 
     {/* Button */}
     <div className="flex justify-center mt-10 mb-10 ">
-      <Link to="/gallery"><Button
-        styles="text-red rounded-[10px] font-montserrat font-medium text-[18px] hover:before:bg-redborder-red-500 relative h-[60px] 
+      <Link to="/gallery">
+        <Button
+          styles="text-red rounded-[10px] font-montserrat font-medium text-[18px] hover:before:bg-redborder-red-500 relative h-[60px] 
         w-50 overflow-hidden border border-red-500 bg-white px-3 text-red-500 shadow-2xl transition-all before:absolute before:bottom-0 
         before:left-0 before:top-0 before:z-0 before:h-full before:w-0 before:bg-red-500 before:transition-all before:duration-500 hover:text-white hover:shadow-red-500 hover:before:left-0 hover:before:w-full"
-        label="View Gallery"
-        SpanStyles="relative z-10"
-        type="button"
-      />
+          label="View Gallery"
+          SpanStyles="relative z-10"
+          type="button"
+        />
       </Link>
     </div>
   </section>
